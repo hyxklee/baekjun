afc70a1752bd319aa53771d9ab10ee81934e9ee6
hyxklee <ewgt1234@naver.com>
2023-10-13 16:00:18 +0900

    Initial Commit

---
+++ baekjun/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  baekjun
+//
+//  Created by 이강혁 on 10/13/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
