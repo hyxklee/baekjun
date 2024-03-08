a5bbae50ab47935c28af03dec1eeb4b21693c323
이강혁 <77369759+hyxklee@users.noreply.github.com>
2024-03-08 10:40:40 +0900

    [Bronze II] Title: 다이얼, Time: 196 ms, Memory: 17660 KB -BaekjoonHub

--- 백준/Bronze/5622. 다이얼/다이얼.java
+++ 백준/Bronze/5622. 다이얼/다이얼.java
@@ -7,24 +7,33 @@ public class Main {
         String s = scanner.nextLine();
 
         int result=0;
-        for(int i =0; i<s.length(); i++){
-            if((s.charAt(i)=='A')|(s.charAt(i)=='B')|(s.charAt(i)=='C')){
-                result += 3;
-            } else if ((s.charAt(i)=='D')|(s.charAt(i)=='E')|(s.charAt(i)=='F')) {
-                result +=4;
-            }else if ((s.charAt(i)=='G')|(s.charAt(i)=='H')|(s.charAt(i)=='I')) {
-                result +=5;
-            }else if ((s.charAt(i)=='J')|(s.charAt(i)=='K')|(s.charAt(i)=='L')) {
-                result +=6;
-            }else if ((s.charAt(i)=='M')|(s.charAt(i)=='N')|(s.charAt(i)=='O')) {
-                result +=7;
-            }else if ((s.charAt(i)=='P')|(s.charAt(i)=='Q')|(s.charAt(i)=='R')|(s.charAt(i)=='S')) {
-                result +=8;
-            }else if ((s.charAt(i)=='T')|(s.charAt(i)=='U')|(s.charAt(i)=='V')) {
-                result +=9;
-            }else if ((s.charAt(i)=='W')|(s.charAt(i)=='X')|(s.charAt(i)=='Y')|(s.charAt(i)=='Z')) {
-                result +=10;
-            }
+        for (int i = 0; i < s.length(); i++) {
+             switch (s.charAt(i)){
+                 case 'A': case 'B': case 'C':
+                     result += 3;
+                     break;
+                 case 'D': case 'E': case 'F':
+                     result += 4;
+                     break;
+                 case 'G': case 'H': case 'I':
+                     result += 5;
+                     break;
+                 case 'J': case 'K': case 'L':
+                     result += 6;
+                     break;
+                 case 'M': case 'N': case 'O':
+                     result += 7;
+                     break;
+                 case 'P': case 'Q': case 'R': case 'S':
+                     result += 8;
+                     break;
+                 case 'T': case 'U': case 'V':
+                     result += 9;
+                     break;
+                 case 'W': case 'X': case 'Y': case 'Z':
+                     result += 10;
+                     break;
+             }
         }
         System.out.println(result);
     }
