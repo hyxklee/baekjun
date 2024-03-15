faaf0d74fda7634dfec2476caddc52564c76414b
이강혁 <77369759+hyxklee@users.noreply.github.com>
2024-03-15 11:15:29 +0900

    [Bronze IV] Title: 모음의 개수, Time: 124 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/1264. 모음의 개수/모음의 개수.java
@@ -0,0 +1,24 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int result = 0;
+
+        while(true) {
+            String s = br.readLine();
+            if(s.equals("#")){
+                break;
+            }
+            for (int i = 0; i < s.length(); i++) {
+                if (s.charAt(i) == 'a' || s.charAt(i) == 'e' || s.charAt(i) == 'i' || s.charAt(i) == 'o' || s.charAt(i) == 'u'
+                ||s.charAt(i) == 'A' || s.charAt(i) == 'E' || s.charAt(i) == 'I' || s.charAt(i) == 'O' || s.charAt(i) == 'U') {
+                    result++;
+                }
+            }
+            System.out.println(result);
+            result = 0;
+        }
+    }
+}
