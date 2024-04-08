d5bab79d6dfdd13978b7e772b6d8870cf92805c9
이강혁 <77369759+hyxklee@users.noreply.github.com>
2024-04-08 16:35:46 +0900

    [Bronze II] Title: 피보나치 수, Time: 204 ms, Memory: 17708 KB -BaekjoonHub

--- 백준/Bronze/2747. 피보나치 수/피보나치 수.java
+++ 백준/Bronze/2747. 피보나치 수/피보나치 수.java
@@ -1,27 +1,28 @@
 import java.util.Scanner;
 
 public class Main {
-    static int fibonacci(int n) {
-        if (n <= 1) {
-            return n;
-        }
-
-        int[] fib = new int[n + 1];
-        fib[0] = 0;
-        fib[1] = 1;
-
-        for (int i = 2; i <= n; i++) {
-            fib[i] = fib[i - 1] + fib[i - 2];
-        }
-
-        return fib[n];
-    }
+    static int[] memo;
 
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
         scanner.close();
 
-        System.out.println(fibonacci(n));
+        memo = new int[n + 1]; // 메모이제이션을 위한 배열 초기화
+        int result = fibonacci(n);
+        System.out.println(result);
+    }
+
+    public static int fibonacci(int n) {
+        if (memo[n] != 0) {
+            return memo[n]; // 이미 계산된 값이라면 바로 반환
+        }
+
+        if (n <= 1) {
+            return n;
+        }
+
+        memo[n] = fibonacci(n - 1) + fibonacci(n - 2); // 계산 결과를 메모이제이션
+        return memo[n];
     }
 }
